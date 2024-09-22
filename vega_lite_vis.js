1fbfe30a0a8f6b252cbcc442ae32de18c6b6b54e
InkyWinky <winkyinky146@gmail.com>
2024-09-22 23:56:21 +1000

    fixed error for not showing map by including github link to file instead of just file name of vg.json

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,3 +1,3 @@
-var vg_1 = "plan_crash_symbol_map.vg.json";
-vegaEmbed("#bar_chart", vg_1).then(function(result) {
+var vg_1 = "https://raw.githubusercontent.com/InkyWinky/w9hw/refs/heads/main/plan_crash_symbol_map.vg.json";
+vegaEmbed("#plane_crash_map", vg_1).then(function(result) {
 }).catch(console.error);
