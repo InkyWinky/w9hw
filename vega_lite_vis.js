96ca2e69ecb66d781ec37b6708857be6c9f58d1b
InkyWinky <winkyinky146@gmail.com>
2024-10-10 17:32:48 +1100

    changed width of bar graph

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,3 +1,6 @@
 var vg_1 = "https://raw.githubusercontent.com/InkyWinky/w9hw/refs/heads/main/plan_crash_symbol_map.vg.json";
 vegaEmbed("#plane_crash_map", vg_1).then(function(result) {
 }).catch(console.error);
+var vg_2 = "https://raw.githubusercontent.com/InkyWinky/w9hw/refs/heads/main/transport_safety.vg.json";
+vegaEmbed("#transport_safety_bar", vg_2).then(function(result) {
+}).catch(console.error);
