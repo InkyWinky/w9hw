0f1dec135e36b64ae5e94c433d8c5d3ad824eba6
InkyWinky <winkyinky146@gmail.com>
2024-10-24 00:13:57 +1100

    a

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -4,7 +4,7 @@ vegaEmbed("#plane_crash_map", vg_1).then(function(result) {
 var vg_2 = "https://raw.githubusercontent.com/InkyWinky/w9hw/refs/heads/main/transport_safety.vg.json";
 vegaEmbed("#transport_safety_bar", vg_2).then(function(result) {
 }).catch(console.error);
-var vg_3 = "https://raw.githubusercontent.com/InkyWinky/w9hw/refs/heads/main/aus_plane_crash_map.json";
+var vg_3 = "https://raw.githubusercontent.com/InkyWinky/w9hw/refs/heads/main/aus_plane_crash_map.vg.json";
 vegaEmbed("#aus_crash_map", vg_3).then(function(result) {
 }).catch(console.error);
 var vg_4 = "https://raw.githubusercontent.com/InkyWinky/w9hw/refs/heads/main/crash_years_line.vg.json";
